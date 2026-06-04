efbb649f73865e5b129318cbffddcbdb9d66994f
BarrelDev <visvam64@gmail.com>
2026-06-04 18:42:47 -0400

    split up UI loading code as suggested by eframe_template

--- twin_ui/src/app.rs
+++ twin_ui/src/app.rs
@@ -1,16 +1,5 @@
-pub struct TwinUIApp {
-    label: String,
-    value: f32
-}
-
-impl Default for TwinUIApp {
-    fn default() -> Self {
-        Self {
-            label: "Hello World!".to_owned(),
-            value: 2.7,
-        }
-    }
-}
+#[derive(Default)]
+pub struct TwinUIApp {}
 
 impl TwinUIApp {
     pub fn new(cc: &eframe::CreationContext<'_>) -> Self {
@@ -18,61 +7,14 @@ impl TwinUIApp {
         // Restore app state using cc.storage (requires the "persistence" feature).
         // Use the cc.gl (a glow::Context) to create graphics shaders and buffers that you can use
         // for e.g. egui::PaintCallback.
-        Default::default()
+        Self::default()
     }
 }
 
 impl eframe::App for TwinUIApp {
    fn ui(&mut self, ui: &mut egui::Ui, frame: &mut eframe::Frame) {
-       egui::Panel::top("top_panel").show_inside(ui, |ui| {
-            egui::MenuBar::new().ui(ui, |ui| {
-                ui.menu_button("File", |ui| {
-                    if ui.button("Quit").clicked() {
-                        ui.send_viewport_cmd(egui::ViewportCommand::Close);
-                    }
-                });
-                ui.add_space(16.0);
-                egui::widgets::global_theme_preference_buttons(ui);
-            });
-       });
-
        egui::CentralPanel::default().show_inside(ui, |ui| {
-            ui.heading("twin_ui");
-            ui.horizontal(|ui| {
-                ui.label("Write something: ");
-                ui.text_edit_singleline(&mut self.label);
-            });
-
-            ui.add(egui::Slider::new(&mut self.value, 0.0..=10.0).text("value"));
-            if ui.button("Increment").clicked() {
-                self.value += 1.0;
-            }
-
-            ui.separator();
-
-            ui.add(egui::github_link_file!(
-                "https://github.com/BarrelDev/twin/blob/master",
-                "Source code."
-            ));
-
-            ui.with_layout(egui::Layout::bottom_up(egui::Align::LEFT), |ui| {
-                powered_by_egui_and_eframe(ui);
-                egui::warn_if_debug_build(ui);
-            });
+           ui.heading("Hello World!");
        });
    }
-}
-
-fn powered_by_egui_and_eframe(ui: &mut egui::Ui) {
-    ui.horizontal(|ui| {
-        ui.spacing_mut().item_spacing.x = 0.0;
-        ui.label("Powered by ");
-        ui.hyperlink_to("egui", "https://github.com/emilk/egui");
-        ui.label(" and ");
-        ui.hyperlink_to(
-            "eframe",
-            "https://github.com/emilk/egui/tree/master/crates/eframe",
-        );
-        ui.label(".");
-    });
 }
