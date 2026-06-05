eaba17f55ca5feb6cfad5908a02fed91bc20697b
BarrelDev <visvam64@gmail.com>
2026-06-04 22:26:41 -0400

    skeleton of tokio task

--- twin_ui/src/lib.rs
+++ twin_ui/src/lib.rs
@@ -2,3 +2,5 @@ use eframe::egui;
 
 mod app;
 pub use app::TwinUIApp;
+
+mod subprocess;
---
+++ twin_ui/src/subprocess.rs
@@ -0,0 +1,29 @@
+use tokio::io::AsyncRead;
+use tokio::task;
+
+enum CMD {
+    Ingest(String),
+    Query(String),
+    RAG(String, u8),
+    Agent(String, u8),
+    Usage,
+    Watch(String),
+    ConfigList,
+    ConfigListModels,
+}
+
+// Massive stub function rn
+// Just placed some nonesense for typecheck
+// need to implement calls to twin cli
+pub fn call_cli(cmd: CMD) -> tokio::task::JoinHandle<String> {
+    match cmd {
+        CMD::Ingest(s) => task::spawn(async {"".to_owned()}),
+        CMD::Query(s) => task::spawn(async {"".to_owned()}),
+        CMD::RAG(s, i) => task::spawn(async {"".to_owned()}),
+        CMD::Agent(s, i) => task::spawn(async {"".to_owned()}),
+        CMD::Usage => task::spawn(async {"".to_owned()}),
+        CMD::Watch(s) => task::spawn(async {"".to_owned()}),
+        CMD::ConfigList => task::spawn(async {"".to_owned()}),
+        CMD::ConfigListModels => task::spawn(async {"".to_owned()})
+    }
+}
