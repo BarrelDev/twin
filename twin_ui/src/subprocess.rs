a751806be17f899ccd6d944e924dd6d5c1c3a5ed
BarrelDev <visvam64@gmail.com>
2026-06-07 20:08:14 -0400

    added ingest_cmd

--- twin_ui/src/subprocess.rs
+++ twin_ui/src/subprocess.rs
@@ -1,5 +1,5 @@
-use tokio::io::AsyncRead;
 use tokio::task;
+use std::process::Command;
 
 pub enum CMD {
     Ingest(String),
@@ -12,12 +12,23 @@ pub enum CMD {
     ConfigListModels,
 }
 
+fn ingest_cmd (s: String) -> String {
+    let raw = Command::new("twin")
+                        .arg(format!("ingest {} --json", (s)))
+                        .output().expect("{}");
+    let stdout = str::from_utf8(&raw.stdout);
+    match stdout {
+        Ok(s) => s.to_owned(),
+        Err(_) => "{}".to_owned(),
+    }
+}
+
 // Massive stub function rn
 // Just placed some nonesense for typecheck
 // need to implement calls to twin cli
 pub fn call_cli(cmd: CMD) -> tokio::task::JoinHandle<String> {
     match cmd {
-        CMD::Ingest(s) => task::spawn(async {"".to_owned()}),
+        CMD::Ingest(s) => task::spawn(async { ingest_cmd(s) }),
         CMD::Query(s) => task::spawn(async {"".to_owned()}),
         CMD::RAG(s, i) => task::spawn(async {"".to_owned()}),
         CMD::Agent(s, i) => task::spawn(async {"".to_owned()}),
